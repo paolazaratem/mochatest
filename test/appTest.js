0844f4e14cf0c3fd24c6533369dafbbd38fe9ccf
Paola Zarate GeekGirl <paolazaratem@gmail.com>
2017-05-23 21:57:00 +0200

    add test with typeOf

--- test/appTest.js
+++ test/appTest.js
@@ -2,58 +2,14 @@ const assert = require('chai').assert;
 //const app = require('../app');
 const sayHello = require('../app').sayHello;
 
-const isPalindrom = require('../app').isPalindrom;
-
 describe('App', function(){
   it('sayHello should return paola', function(){
-    assert.equal(sayHello(),'paola');
+    let result = sayHello();
+    assert.equal(result,'paola');
   });
 
   it('sayHello should return type string',function(){
-
-  });
-});
-
-describe('2', function(){
-  it('should return boolean when string is empty', function(){
-    assert.equal(isPalindrom(""), true);
-  });
-});
-
-describe('3', function(){
-  it('should return true when string have size is equal 1', function(){
-    assert.equal(isPalindrom("a"), true);
-  });
-});
-
-describe('4', function(){
-  it('should return true when size of string is odd is palindrom', function(){
-    assert.equal(isPalindrom("aba"), true);
-  });
-});
-
-describe('5', function(){
-  it('should return false when size of string is odd and is not palindrom', function(){
-    assert.equal(isPalindrom("12341"), false);
-  });
-});
-
-describe('6', function(){
-  it('should return true when size of string is even is palindrom', function(){
-    assert.equal(isPalindrom("1221"), true);
-  });
-});
-
-describe('7', function(){
-  it('should return false when size of string is even is not palindrom', function(){
-    let result = isPalindrom("124431");
-    assert.equal(result, false);
-  });
-});
-
-describe('8', function(){
-  it('should return true when string is very big is palindrom', function(){
-    assert.equal(isPalindrom("111116111111"), false);
-    assert.equal(isPalindrom("12345654321"), true);
+    let result = sayHello();
+    assert.typeOf(result,'string');
   });
 });
